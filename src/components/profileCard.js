708c3f46a4f6ed11bfe38dcb57707d26f10dd6a9
Salia-Monica <saliamonica78@gmail.com>
2022-03-22 11:12:01 +0000

    react evnt

--- src/components/profileCard.js
+++ src/components/profileCard.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import{useState} from "react"
 
 function ProfileCard({writer}) {
+  const[showBio, setShowBio] =useState(false);
+  const handleClick = (bioData)=>{
+    setShowBio(!showBio);
+  };
+
   return (
-    <div className='card'>
+    
+
+    <div className='card'> 
+    <div>
+    {setShowBio ? ( <div className='biowrap'>
+    <p className='bio'>{writer.bio}</p>
+    ):(
+    <img src={`images/${writer.avatar}.png`}
+    height="300px" width="300px" alt={writer.img}/>
+    )}
+      </div>
+   
+    </div>
 <img src={`images/${writer.avatar}.png`}
 height="300px" width="300px" alt={writer.img}/>
 
@@ -11,7 +29,7 @@ height="300px" width="300px" alt={writer.img}/>
 <h3>{writer.name}</h3>
 <p>{writer.email}</p>
 <p>{writer.phone}</p>
-<button className='actionBtn' >Read Bio</button>
+<button className='actionBtn'onClick={()=>handleClick(writer.bio)} >Read Bio</button>
     </div>
   )
 }
